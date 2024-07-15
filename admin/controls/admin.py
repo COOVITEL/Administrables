fc158b3e708d061c67b4d4a88bfd63b4613f7491
COOVITEL <direcciontop@coovitel.coop>
2024-07-15 16:42:34 -0500

    add sociales, fidelidad and asociados types

--- admin/controls/admin.py
+++ admin/controls/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import TasasCDAT, TasasCooviahorro
+from .models import TasasCDAT, TasasCooviahorro, Sociales
 
 admin.site.register(TasasCDAT)
 admin.site.register(TasasCooviahorro)
+admin.site.register(Sociales)
