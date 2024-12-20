54c5b61d2617cd81895ea4d74ab2692e830d603b
LAURA ALEJANDRA CABRERA CORREDOR <LCABRERA@coovitel.local>
2024-12-20 11:15:17 -0500

    Create funtion deleteSucursal and deleteAsesor

--- admin/asesores/urls.py
+++ admin/asesores/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path('asesorCreate/', views.asesorCreate, name='asesorCreate'),
     path('sucursalList/', list.sucursalList, name='sucursalList'),  
     path('asesoresList/', list.asesoresList, name='asesoresList'),
+    path('eliminar-asesor/<int:asesor_id>/', views.eliminar_asesor, name='eliminar_asesor'),
+    path('eliminar-sucursal/<int:sucursal_id>/', views.eliminar_sucursal, name='eliminar_sucursal'),
 ]
--- admin/asesores/views.py
+++ admin/asesores/views.py
@@ -2,6 +2,9 @@ from django.shortcuts import render, redirect
 from django.contrib import messages
 from .forms import SucursalForm, AsesoresForm
 from .models import Sucursal, Asesores
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.shortcuts import get_object_or_404
 
 def sucursalCreate(request):
     if request.method == 'POST':
@@ -13,7 +16,7 @@ def sucursalCreate(request):
     else:
         form = SucursalForm()
 
-    sucursales = Sucursal.objects.all()  # Obtener todas las sucursales
+    sucursales = Sucursal.objects.all()  
     return render(request, 'sucursal.html', {'form': form, 'sucursales': sucursales})
 
 
@@ -27,5 +30,21 @@ def asesorCreate(request, ):
     else:
         form = AsesoresForm()
 
-    asesores = Asesores.objects.all()  # Obtener todos los asesores
+    asesores = Asesores.objects.all()  
     return render(request, 'asesores.html', {'form': form, 'asesores': asesores})
+
+@csrf_exempt
+def eliminar_asesor(request, asesor_id):
+    if request.method == 'DELETE':
+        asesor = get_object_or_404(Asesores, id=asesor_id)
+        asesor.delete()
+        return JsonResponse({'message': 'Asesor eliminado correctamente.'}, status=200)
+    return JsonResponse({'error': 'Método no permitido.'}, status=405)
+
+@csrf_exempt
+def eliminar_sucursal(request, sucursal_id):
+    if request.method == 'DELETE':
+        sucursal = get_object_or_404(Sucursal, id=sucursal_id)
+        sucursal.delete()
+        return JsonResponse({'message': 'Sucursal eliminada correctamente.'}, status=200)
+    return JsonResponse({'error': 'Método no permitido.'}, status=405)
